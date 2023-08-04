15ca33d2038d2a390f7cf73c33c922e3ecf50965
Livia2710 <livia.sesi166@gmail.com>
2023-08-04 15:12:07 -0300

    ayhf

---
+++ exemplos/2_TIPOS DE DADOS/1 number/script.js
@@ -0,0 +1,9 @@
+console.log(5);
+console.log(35.7);
+console.log(-75);
+console.log(1000.45);
+
+console.log( typeof 5); //number
+console.log( typeof 35.7); //number
+console.log( typeof -75); // number
+console.log( typeof 1000.45); //number
---
+++ exemplos/2_TIPOS DE DADOS/4__string/script.js
@@ -0,0 +1,8 @@
+console.log( "Meu nome é Valmir");//Todas são tratadas como string
+console.log( 'Desenvolvimento no Senai');
+
+//Template literais (crase) ao lado da tecla P.
+console.log (`teste`);
+
+console.log( typeof "infinity")//string
+console.log(typeof infinity)//number
