a18d42e9ca38f3375e0c53cabae1ea6593b47855
Livia2710 <livia.sesi166@gmail.com>
2023-11-16 11:18:48 -0300

    calculadora

--- exemplos/Calculadora dark/script.js
+++ exemplos/Calculadora dark/script.js
@@ -1,1 +1,49 @@
 /* Digite o JS aqui */
+
+//Seleciona o elemento HTML com id 'inputBox' e armazena-o na variável 'input'
+let input = document.getElementById('inputBox');
+
+//Seleciona todos os elemento HTML do tipo button e armazena-o em 'buttin'
+let buttons = document.querySelectorAll('button');
+
+//Inicializa uma string vazia para armazenar a entrada do usuario
+let string = "";
+
+//Converte a coleção de botões em um array para possamos percorrer sobre eles
+let arr = Array.from(buttons);
+
+//Percorre sobre cada botão no array 'arr'
+arr.forEach(button => {
+    //Adiciona um ouvinte de eventos de clique a cada botão
+    button.addEventListener('click',(e) => {
+        //Verifica se o texto dentro do botão clicado é '='
+        if (e.target.innerHTML == '=') {
+            string = eval(string);
+
+            //Define o valor da caixa de entrada 'input' como a nova string calculada
+            input.value = string;
+        }
+        //Verfica se o texto dentro do botão clicado é 'AC' (Clear All)
+        else if (e.target.innerHTML == 'AC') {
+            //Limpa a string, redefinindo-a como uma string vazia
+            string = "";
+            //Define o valor da caixa de entrada 'input' como string vazia.
+            input.value = string;
+        }
+         //Verifica se o texto dentro do botão clicado é DEL (Delete)
+         else if (e.target.innerHTML == 'DEL') {
+         //Remove o último caractere da string
+        string = string.substring(0,string.length - 1);
+        //Define o valor da caixa de entrada 'input' como a nova string após a remoção
+        input.value = string;
+         }
+         //Se nenhum dos casos anteriores corresponder, significa que é um digito ou operador.
+         else {
+            //Adiciona o texto do botão clicado á string de entrada.
+            string += e.target.innerHTML;
+            //Define o valor da caixa de entrada 'input' como a nova string de entrada.
+            input.value = string;
+         }
+
+    })
+})
